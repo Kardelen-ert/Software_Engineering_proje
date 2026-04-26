469763a0e9a05c99a6008412d317092f321abca1
glsmdpr <gulsum.aydin399@gmail.com>
2026-04-26 18:36:00 +0300

    profil goruntuleme eklendi

--- Backend/app/services/auth_services.py
+++ Backend/app/services/auth_services.py
@@ -1,8 +1,13 @@
 import hashlib
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from typing import Optional
-from app.core.config import settings    
+from app.core.config import settings   
+from app.models.user import User 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.core.database import get_db
 
 
 def get_password_hash(password: str) -> str:
@@ -29,4 +34,28 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         settings.SECRET_KEY, 
         algorithm=settings.ALGORITHM
     )
-    return encoded_jwt          
+    return encoded_jwt     
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Geçersiz kimlik bilgileri",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Token'ı senin config dosyandaki ayarlarla çözüyoruz
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+        
+    # Veritabanında o username'e sahip kullanıcıyı buluyoruz
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credentials_exception
+        
+    return user
